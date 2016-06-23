7decde67fc2c0474df203ecb8eb1c776a1706957
lesguillemets <lesguillemets@users.noreply.github.com>
2016-06-23 23:30:29 +0900

    clean up

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,27 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import YOLP
-import YOLP.Base
+import YOLP.Base (toRequest)
 import YOLP.Weather.Query
-import YOLP.Weather.Result.Internal
+import YOLP.Weather.Result
 import qualified YOLP.Geocoder.Query as G
-import YOLP.Geocoder.Result
-import Data.Aeson
+import YOLP.Geocoder.Result (getFirstCandidate)
 import Data.Text (Text)
 import qualified Data.Text.IO as TI
 import Network.HTTP.Simple (httpLBS)
 import Network.HTTP.Conduit (responseBody)
-import System.Exit
-import qualified Data.ByteString.Lazy.Char8 as BLC
 import Data.Maybe (fromJust)
--- TODO : check how to do this
-assertJust :: String -> Maybe a -> IO ()
-assertJust msg (Just x) = putStrLn ("success:" ++ msg)
-assertJust msg Nothing = putStrLn ("fail on" ++ msg) *> exitFailure
 
 main :: IO ()
 main = do
-    (httpLBS . toRequest $ baseQuery `withCoords` (135,35))
-        >>= mapM_ print . getWeathers . fromJust . decode . responseBody
     (httpLBS . toRequest $ G.baseQuery `G.withQuery` "明石市")
         >>= print . fromJust . getFirstCandidate . responseBody
     YOLP.getRainAtWithPast "京都市" >>=
@@ -29,4 +20,3 @@ main = do
                          (Right (l,ws)) -> do
                              TI.putStrLn l
                              mapM_ print ws)
-
