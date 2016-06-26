8e51c3a966f96452e6e28b3da686cbbe135b2a79
lesguillemets <lesguillemets@users.noreply.github.com>
2016-06-27 00:04:21 +0900

    work at your place

---
+++ src/Secrets.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Secrets
+    (myAppID) where
+
+import Data.ByteString (ByteString)
+
+
+myAppID :: ByteString
+myAppID = "dj0zaiZpPXNva2VzMFVFWms0NSZzPWNvbnN1bWVyc2VjcmV0Jng9MmI-"
+
